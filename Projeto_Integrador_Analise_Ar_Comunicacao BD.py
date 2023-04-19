201fed1c16a2c5f051cc7a677c80467b7fab7eea
tS1lva <tiago.ps2@puccampinas.edu.br>
2023-04-18 21:24:14 -0300

    modificado cursos conexao

--- Projeto_Integrador_Analise_Ar_Comunicacao BD.py
+++ Projeto_Integrador_Analise_Ar_Comunicacao BD.py
@@ -3,20 +3,19 @@ import getpass
 
 #pw = getpass.getpass("Enter password: ")
 
-connection = oracledb.connect(
+con = oracledb.connect(
     user='bd240223112',
     password='Oosxb4',
     dsn='172.16.12.14:1521/XE')
 
 print("Successfully connected to Oracle Database")
 
-cursor = connection.cursor()
-cursor.execute("select * from AMOSTRAS order by deptno")
-res = cursor.fetchall()
-cursor.close()
-connection.close()
 
-
+cur = con.cursor()
+cur.execute("select * from AMOSTRAS")
+res = cur.fetchall()
+for row in res:
+    print(row)
 
 
 
